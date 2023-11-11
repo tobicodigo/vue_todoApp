475725d49b0a59b04b883bc1658d1d51436675f3
tobi <t.goral@gmx.net>
2023-11-11 09:25:15 +0100

    first upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vue_todoApp
+
+clone gibhub repository and run:
+
+npm run dev
---
+++ src/views/HomePage.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>My Tasks</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+     
+
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Inbox</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+    
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+import { ref } from 'vue';
+
+
+</script>
